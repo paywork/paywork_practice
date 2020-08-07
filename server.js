1608af6a49bd5395c3bb19a996d3ba661f8a28ab
Jean <start@paywork.io>
2020-08-07 14:17:34 +0900

    설치

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+const userRoute = require('./routes/user')
 
 //미들웨어
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use('/user',userRoute)
 
 
-
-const PORT = 3838
+const PORT = 3838;
 
 app.listen(PORT, console.log'server started!')
